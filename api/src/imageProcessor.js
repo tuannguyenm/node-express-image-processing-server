ab794a9acd1918abd625278e51d182d74a1cc168
tuannguyen <tuan.nguyenminh@nashtechglobal.com>
2020-07-06 11:03:14 +0700

    Creating the parent thread

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -2,7 +2,12 @@ const path = require("path");
 const { Worker, isMainThread } = require("path");
 
 const pathToResizeWorker = path.resolve(__dirname, "resizeWorker.js");
-function imageProcessor() {
+const pathToMonochromeWorker = path.resolve(__dirname, "monochromeWorker.js");
+
+function imageProcessor(filename) {
+    const sourcePath = uploadPathResolver(filename);
+    const resizedDestination = uploadPathResolver("resized-" + filename);
+    const monochromeDestination = uploadPathResolver("monochrome-" + filename);
     return new Promise((resolve, reject) => {
         if (isMainThread) {
 
@@ -10,6 +15,10 @@ function imageProcessor() {
             reject(new Error("not on main thread"))
         }
     });
+};
+
+function uploadPathResolver(filename) {
+    return path.resolve(__dirname, '../uploads', filename);
 };
 
 module.exports = imageProcessor;
